c63ed19c70b24c429a9cff6d28b526fbb70097af
D303340 <154211306+D303340@users.noreply.github.com>
2024-06-06 11:19:28 +0200

    OrderProduct.cs aangemaakt
    een OrderProduct.cs aangemaakt voor ticked B2

---
+++ PRA_B4_FOTOKIOSK/models/OrderedProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRA_B4_FOTOKIOSK.models
+{
+    public class OrderedProduct
+    {
+        public static int FotoID {  get; set; }
+        public static string ProductName { get; set; }
+        public static int Amount { get; set; }
+        public static float TotalPrice { get; set; }
+    }
+}
